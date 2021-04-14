f04b9d6980387d78a755d1e4dd769cc4e7d6d654
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-04-14 23:04:38 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import Header from "./components/Header/Header";
+import Header from "./components/Header";
+import SideBar from "./components/SideBar";
+import HomePage from "./pages/HomePage";
+import { Row } from './components/Styles';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Row>
+        <SideBar />
+        <HomePage />
+      </Row>
     </div>
   );
 }
