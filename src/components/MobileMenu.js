edf0b167ded433a12b019a3b81c193b8f4d1d691
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-05-13 22:29:05 +0300

    mobile menu component added.

---
+++ src/components/MobileMenu.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+import MenuItem from "./MenuItem";
+import { mobileMenuItems } from '../lib/menuItems';
+
+
+const MenuContainer = styled.div`
+  display:flex;
+  justify-content:space-around;
+  background-color: white;
+  width: 100%;
+  position: fixed;
+  z-index: 10;
+  bottom: 0;
+
+  @media (min-width: 640px) {
+    display: none;
+  }
+`;
+
+const MobileMenu = () => {
+  return (
+    <MenuContainer>
+		{
+			mobileMenuItems.map((item, i)=> {
+				return(
+					<MenuItem 
+					key={i}
+					Logo={item.logo}
+					text={item.text}
+					/>
+				)
+			})
+		}
+
+    </MenuContainer>
+  );
+};
+
+export default MobileMenu;
