229bd87f33b25e278259f72c2b6744ac8b01b026
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-05-13 22:33:18 +0300

    homepage videos added dynamicly.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,30 +2,30 @@ import React from "react";
 import VideoCover from "../components/VideoCover";
 import Slider from "../components/Slider";
 import styles from "./HomePage.module.css";
-
+import { useHomeVideos } from "../context/homeVideoContext";
 
 const HomePage = () => {
+  const { homeVideos, recommendVideos } = useHomeVideos();
 
-	
+  console.log(recommendVideos);
+  if (!homeVideos) return <h2>Loading...</h2>;
 
   return (
     <div className={styles.homePage}>
       <Slider>
-        <VideoCover />
-        <VideoCover />
-        <VideoCover />
-        <VideoCover />
-        <VideoCover />
-        <VideoCover />
-        <VideoCover />
+        {recommendVideos.map((video) => {
+          return <VideoCover large video={video} />;
+        })}
       </Slider>
-      <Slider>
-        <VideoCover large />
-        <VideoCover large />
-      </Slider>
-      <Slider>
-        <VideoCover />
-      </Slider>
+      {homeVideos.map((category) => {
+        return (
+          <Slider>
+            {category.videos.map((video) => {
+              return <VideoCover video={video} />;
+            })}
+          </Slider>
+        );
+      })}
     </div>
   );
 };
