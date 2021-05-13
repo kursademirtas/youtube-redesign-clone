04d79dd036dd0063469a58760de2d2c9b29165c5
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-05-13 22:31:51 +0300

    add dynamic url for videos.

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -3,20 +3,19 @@ import styled from "styled-components";
 
 const Player = styled.video`
   width: 100%;
-  max-width:1000px;
+  max-width: 1000px;
   height: 500px;
   object-fit: cover;
   border-radius: 12px;
   margin: 0 auto;
 `;
 
-const VideoPlayer = () => {
+const VideoPlayer = ({ video }) => {
+  if (!video) return <h2>Loading..</h2>;
+  const { url } = video;
+
   return (
-    <Player
-      controls
-      preload="none"
-      poster="https://images.unsplash.com/photo-1620376165025-a807786689fa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
-    >
+    <Player controls preload="none" poster={`http://localhost:1337${url}`}>
       <source src="video.mp4" type="video/mp4"></source>
     </Player>
   );
