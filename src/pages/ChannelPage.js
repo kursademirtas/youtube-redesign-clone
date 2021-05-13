6e7959b3d46772bf615ee71d51aaf9496adad2a9
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-05-13 22:35:48 +0300

    fixed useEffect, for refetch data and some styles changed.

--- src/pages/ChannelPage.js
+++ src/pages/ChannelPage.js
@@ -1,67 +1,78 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import axios from 'axios';
-import { useParams } from 'react-router-dom'
-import { Avatar, Row } from '../components/Styles';
-import UserAvatar from '../components/UserAvatar';
-import SubscribeButton from '../components/SubscribeButton';
-import ChannelNav from '../components/ChannelNav';
-import Slider, { SliderContainer } from '../components/Slider';
-import VideoCover from '../components/VideoCover';
-import HighlightedVideoCover from '../components/HighlightedVideoCover';
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { Row } from "../components/Styles";
+import UserAvatar from "../components/UserAvatar";
+import SubscribeButton from "../components/SubscribeButton";
+import ChannelNav from "../components/ChannelNav";
+import Slider from "../components/Slider";
+import VideoCover from "../components/VideoCover";
+import HighlightedVideoCover from "../components/HighlightedVideoCover";
 
 const Layout = styled.div`
-	width:100%;
-	display:grid;
-`
+  max-width: calc(100vw - 200px);
+  margin-left: 200px;
+
+  @media only screen and (max-width: 1023px) {
+    margin-left: 0;
+    max-width: 100vw;
+  }
+`;
 const Banner = styled.img`
-	width:90%;
-	height:200px;
-	margin:1rem 0;
-	
-`
+  width: 100%;
+  height: 200px;
+  margin: 1rem 0;
+`;
 const ChannelPage = () => {
+  const { channelID } = useParams();
+  const [channelData, setChannelData] = useState(null);
 
-	const { channelID } = useParams();
-	const  [channelData, setChannelData ] = useState(null)
-	
-	const fetchChannelByID = async (id) => {
-		axios.get(`http://localhost:1337/channels/${id}`)
-			 .then(function (response) {
-				 setChannelData(response.data)
-			 });
-	  };
-	
-	  useEffect(() => {
-		fetchChannelByID(channelID)
-	  },[])
+  const fetchChannelByID = async (id) => {
+    axios.get(`http://localhost:1337/channels/${id}`).then(function (response) {
+      setChannelData(response.data);
+    });
+  };
 
-	  if(!channelData) return <h2>Loading...</h2>
- 	  const { banner, about, name, verified, profile_picture, subscribers, videos } = channelData;
-	  
-	   const indexOfHighLigthVideo =  Math.floor(Math.random() * videos.length);
-	   const highLightVideo = videos[indexOfHighLigthVideo];
+  useEffect(() => {
+    fetchChannelByID(channelID);
+  }, [channelID]);
 
-	return (
-		<Layout>
-			<Banner src={`http://localhost:1337${banner.url}`} alt=""/>
-			<Row>
-			<UserAvatar 
-				url={profile_picture.url} 
-				name={name} 
-				subscribers={subscribers} 
-			/>
-			<SubscribeButton subscribers={'2k'} />
-			</Row>
-			<ChannelNav />
-			<HighlightedVideoCover video={highLightVideo} />
-			<Slider withOutName>
-				{videos.map(video => {
-					return <VideoCover video={video}/>
-				})}
-			</Slider>
-		</Layout>
-	)
-}
+  if (!channelData) return <h2>Loading...</h2>;
 
-export default ChannelPage
+  const {
+    banner,
+    about,
+    name,
+    verified,
+    profile_picture,
+    subscribers,
+    videos,
+  } = channelData;
+
+  const indexOfHighLigthVideo = Math.floor(Math.random() * videos.length);
+  const highLightVideo = videos[indexOfHighLigthVideo];
+
+  return (
+    <Layout>
+      <Banner src={`http://localhost:1337${banner.url}`} alt="" />
+      <Row>
+        <UserAvatar
+          url={profile_picture.url}
+          name={name}
+          subscribers={subscribers}
+        />
+        <SubscribeButton subscribers={"2k"} />
+      </Row>
+      <ChannelNav />
+      <HighlightedVideoCover video={highLightVideo} />
+      <Slider withOutName>
+        {videos.map((video) => {
+          return <VideoCover video={video} />;
+        })}
+      </Slider>
+    </Layout>
+  );
+};
+
+export default ChannelPage;
