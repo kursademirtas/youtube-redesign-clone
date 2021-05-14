e414292faa7b128c56db54e9a29e377cd71cf895
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-05-14 22:56:33 +0300

    small style bugs fixed.

--- src/components/ChannelInfo.js
+++ src/components/ChannelInfo.js
@@ -1,21 +1,23 @@
 import React from "react";
 import styled from "styled-components";
-import { Avatar, NameTitle, Text } from "./Styles";
+import { Avatar, NameTitle } from "./Styles";
 import SubscribeButton from "./SubscribeButton";
 
 const InfoContainer = styled.div`
   max-width: 100%;
-  display: grid;
-  grid-template-columns: 1fr 6fr 1fr;
-  padding: 1rem;
+  display: flex;
+  justify-content:space-between;
+  padding:1rem 0;
 `;
 const InfoBody = styled.div`
-  width: 80%;
+  max-width: 70%;
   padding: 0.5rem;
 `;
+const Row = styled.div`
+  display:flex;
+`
 const About = styled.p`
   font-size: 14px;
-
   @media only screen and (max-width: 600px) {
     display: none;
   }
@@ -28,6 +30,7 @@ const ChannelInfo = ({ author }) => {
 
   return (
     <InfoContainer>
+      <Row>
       <Avatar
         size={"60px"}
         src={`http://localhost:1337${profile_picture.url}`}
@@ -36,6 +39,7 @@ const ChannelInfo = ({ author }) => {
         <NameTitle>{name}</NameTitle>
         <About>{about}</About>
       </InfoBody>
+      </Row>
       <SubscribeButton subscribers={subscribers} />
     </InfoContainer>
   );
--- src/components/Styles.js
+++ src/components/Styles.js
@@ -2,36 +2,35 @@ import styled from "styled-components";
 
 export const Row = styled.div`
   display: flex;
-  justify-content:space-between;
-  align-items:center;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const Avatar = styled.img`
-  width:${props =>props.size};
-  height:${props =>props.size};
-  border-radius:${props =>props.size};
-  background-color:orange;
-`
+  width: ${(props) => props.size};
+  height: ${(props) => props.size};
+  border-radius: ${(props) => props.size};
+  background-color: orange;
+`;
 
 export const NameTitle = styled.span`
   font-weight: bold;
   font-size: 26px;
   line-height: 33px;
-  color: #19202C;
-
-`
+  color: #19202c;
+`;
 
 export const Text = styled.p`
   font-size: 13px;
   line-height: 16px;
-  color: #C2C2C2;
-`
+  color: #c2c2c2;
+`;
 export const Main = styled.div`
   display: flex;
-  justify-content:space-between;
-  align-items:center;
-  width:100%;
-  margin-top:80px;
-  overflow:hidden;
-  position:relative;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-top: 80px;
+  overflow: hidden;
+  position: relative;
 `;
